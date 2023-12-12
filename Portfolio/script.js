4a1e712eb9ce2e28d241c2a9dcfb15819e14ed59
Subham Bhattacharya <87707182+Subhampops@users.noreply.github.com>
2023-12-12 14:33:18 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+    strings:["Machine Learning","Full Stack Development","Ethical Hacking","IOT","Cyber Security","Sound Editing","Artificial Intelligence","Computer Hardware","Robotics"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
